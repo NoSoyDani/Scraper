b649acc556304dca817a2e95a7bffdbc7e6e5d7e
I'm NOT Dani <61089532+NoSoyDani@users.noreply.github.com>
2020-02-18 09:59:40 +0100

    Add files via upload

---
+++ requirements.bat
@@ -0,0 +1,20 @@
+pip install threading
+
+pip install os
+
+pip install webbrowser
+
+pip install bs4
+
+pip install BeautifulSoup4
+
+pip install requests
+
+pip install colorama
+
+pip install wget
+
+pip install time
+
+pip install playsound
+
